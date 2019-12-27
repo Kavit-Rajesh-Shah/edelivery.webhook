29373aa42c61b01c38dfe4c5cd76f8b577abf610
kavit.shah <kavitsh@gmail.com>
2019-12-28 00:33:36 +0100

    Changed Server Port and Test Profile

--- src/test/java/io/edelivery/webhook/EDeliveryWebhookApplicationTests.java
+++ src/test/java/io/edelivery/webhook/EDeliveryWebhookApplicationTests.java
@@ -1,13 +1,20 @@
 package io.edelivery.webhook;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import io.edelivery.webhook.services.RabbitMQSender;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EDeliveryWebhookApplicationTests {
 
+	@Autowired
+	private RabbitMQSender rabbitMQSender;
+	
 	@Test
 	void contextLoads() {
 	}
-
 }
